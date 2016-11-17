132db03a0078577f2cc4df66ff5636d602bbdba6
Nicole Shelton <nshelton@basecampcodingacademy.org>
2016-11-17 15:56:45 -0600

    changed sizes to match prices

--- brokedata.js
+++ brokedata.js
@@ -5,161 +5,160 @@ skillsdata = {
     "McDonald's": {
       "Main": {
         "Chicken": {
-          'McChicken': [25],
-          '4Pc. Nuggets': [25],
-          '20Pc. Nuggets': [25]
+          'McChicken': [10],
+          '4Pc. Nuggets': [20],
+          '20Pc. Nuggets': [40]
         },
         "Beef": {
-          'McDouble': [25],
-          'Double Cheeseburger': [25],
-          'Bacon McDouble': [25]
+          'McDouble': [12],
+          'Double Cheeseburger': [13],
+          'Bacon McDouble': [30]
         },
         "Other": {
-          'Salad': [25],
-          "Apple Slices": [25]
+          'Salad': [10],
+          "Apple Slices": [10]
         }
       },
       "Sides": {
         "Fries": {
-          'Small': [25],
-          'Medium': [25],
-          'Large': [25]
+          'Small': [14],
+          'Medium': [18],
+          'Large': [19]
         },
       },
       "Drinks": {
-        "Fountain Drink": [25],
-        "Tea": [25],
+        "Fountain Drink": [12],
+        "Tea": [10],
         "Other": {
-          'Bottled Water': [25],
-          'Apple Juice': [25],
-          'McCafe': [25],
-          'Chocolate Milk': [25]
+          'Bottled Water': [10],
+          'Apple Juice': [12],
+          'McCafe': [10],
+          'Chocolate Milk': [12]
         }
       },
       "Deserts": {
-        '3 Cookies': [25],
-        'Vanilla Cone': [25]
+        '3 Cookies': [10],
+        'Vanilla Cone': [10]
       }
     },
     "Wendy's": {
       "Main": {
         "Chicken": {
-          'Chicken Sandwich': [25],
-          'Chicken Wrap': [25],
-          'Chicken Nuggets': [25]
+          'Chicken Sandwich': [10],
+          'Chicken Wrap': [18],
+          'Chicken Nuggets': [10]
         },
         "Beef": {
-          'Jr. Burger Deluxe': [25],
-          'Jr. Bacon Burger': [25],
-          'Double Stack': [25],
-          'Cheeseburger': [25],
-          'Hamburger': [25]
+          'Jr. Burger Deluxe': [19],
+          'Jr. Bacon Burger': [20],
+          'Double Stack': [20],
+          'Cheeseburger': [19],
+          'Hamburger': [19]
         }
       },
       "Sides": {
         "Fries": {
-          'Small': [25],
-          'Medium': [25],
-          'Large': [25]
+          'Small': [17],
+          'Medium': [20],
+          'Large': [22]
         },
         "Side Salad": {
-          'Garden': [25],
-          'Caesar': [25]
+          'Garden': [15],
+          'Caesar': [15]
         },
-        'Chili': [25]
+        'Chili': [20]
       },
       "Drinks": {
-        "Fountain Drink": [25],
-        "Tea": [25],
+        "Fountain Drink": [19],
+        "Tea": [19],
         "Other": {
-          'Water': [25],
-          'Coffee': [25]
+          'Water': [10],
+          'Coffee': [10]
         }
       },
       "Desserts": {
         'Frosty': {
-          'Chocolate': [25],
-          'Vanilla': [25]
+          'Chocolate': [10],
+          'Vanilla': [10]
         }
       }
     },
     "Taco Bell": {
       "Main": {
         "Tacos": {
-          'Soft Taco': [25],
-          'Hard Taco': [25]
+          'Soft Taco': [10],
+          'Hard Taco': [10]
         },
         "Burritos": {
-          "Cheesy Burrito": [25],
-          'Beefy Fritos Burrito': [25],
-          'Beefy Nacho Burrito': [25]
+          "Cheesy Burrito": [10],
+          'Beefy Fritos Burrito': [10],
+          'Beefy Nacho Burrito': [10]
         },
         'Other': {
-          'Beef Quesadilla': [25],
-          'Chicken Quesadilla': [25]
+          'Beef Quesadilla': [10],
+          'Chicken Quesadilla': [10]
         }
       },
       "Sides": {
-        "Chips & Cheese": [25],
-        'Chips & Salsa': [25],
-        'Doritos': [25]
+        "Chips & Cheese": [14],
+        'Chips & Salsa': [15],
+        'Doritos': [10]
       },
       "Drinks": {
-        "Fountain Drink": [25],
-        "Tea": [25],
+        "Fountain Drink": [14],
+        "Tea": [14],
         "Other": {
-          'Water': [25],
-          'Coffee': [25]
+          'Water': [14],
+          'Coffee': [15]
         }
       },
       "Desserts": {
-          'Cinnabon Delights': [25],
-          'Cinnamon Twist': [25],
-          'Caramel Apple Empanada': [25]
+          'Cinnabon Delights': [10],
+          'Cinnamon Twist': [10],
+          'Caramel Apple Empanada': [10]
       }
     },
     "Sonic": {
       "Main": {
         "Beef": {
           'Burgers': {
-            'Jr. Burger': [25],
-            'Jr. Deluxe Burger': [25],
-            'Jr. Double Burger': [25]
+            'Jr. Burger': [14],
+            'Jr. Deluxe Burger': [17],
+            'Jr. Double Burger': [20]
           },
           'Hotdogs': {
-            'Chilli Cheese Coney': [25],
-            'Corndog': [25]
+            'Chilli Cheese Coney': [20],
+            'Corndog': [13]
           }
         },
         "Chicken": {
-          "Chicken Strip Sandwich": [25],
-          'Chicken Strips': [25],
-          'Boneless Wings': [25]
+          "Chicken Strip Sandwich": [14]
         }
       },
       "Sides": {
         "Tots": {
-          'Regular': [25],
-          'Chilli Cheese': [25]
+          'Regular': [12],
+          'Chilli Cheese': [23]
         },
         "Fries": {
-          'Regular': [25],
-          'Chilli Cheese': [25]
+          'Regular': [12],
+          'Chilli Cheese': [23]
         },
-        "Mozzarella Sticks": [25],
-        'Onion Rings': [25]
+        "Mozzarella Sticks": [13],
+        'Onion Rings': [13]
       },
       "Drinks": {
-        "Fountain Drink": [25],
-        "Tea": [25],
+        "Fountain Drink": [12],
+        "Tea": [12],
+        "Slush": [12],
         "Other": {
-          'Limeade': [25],
-          'Water': [25]
+          'Limeade': [17],
+          'Water': [10]
         }
       },
       "Deserts": {
-        'Shakes': [25],
-        'Blasts': [25],
+        'Shakes': [20],
+        'Blasts': [30],
       }
     }
   }
